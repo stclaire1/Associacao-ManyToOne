d5a50868793b40aa3a37a86a591f47e7726548c8
Ana Clara Custodio <dev.anaccustodio@gmail.com>
2024-07-18 21:14:14 -0300

    update BookRepository.java package import

--- src/main/java/br/edu/iftm/manytoone/repository/BookRepository.java
+++ src/main/java/br/edu/iftm/manytoone/repository/BookRepository.java
@@ -1,8 +1,8 @@
-package iftm.edu.br.biblioteca.repository;
+package br.edu.iftm.manytoone.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import iftm.edu.br.biblioteca.domain.Book;
+import br.edu.iftm.manytoone.domain.Book;
 
 //a notação aqui faz o laço com o @autowired na main, indicando que a dependência pode ser injetada lá
 @Repository
