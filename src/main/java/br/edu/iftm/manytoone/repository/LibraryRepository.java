28e3929c3e37c63db70587c44cf5b2629ede6d9a
Ana Clara Custodio <dev.anaccustodio@gmail.com>
2024-07-18 21:15:01 -0300

    update LibraryRepository.java package import

--- src/main/java/br/edu/iftm/manytoone/repository/LibraryRepository.java
+++ src/main/java/br/edu/iftm/manytoone/repository/LibraryRepository.java
@@ -1,8 +1,8 @@
-package iftm.edu.br.biblioteca.repository;
+package br.edu.iftm.manytoone.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import iftm.edu.br.biblioteca.domain.Library;
+import br.edu.iftm.manytoone.domain.Library;
 
 @Repository
 public interface LibraryRepository extends CrudRepository<Library, Long> {}
